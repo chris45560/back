143944690051173c96f33b784f513185a787a0ee
christophe <christophe.hebert45@gmail.com>
2019-01-21 02:46:53 +0100

    Test Technique Back - 3ème étape - Ajout de commentaires

--- drupal-8.6.7/modules/custom/idix_back/src/Form/IdixbackForm.php
+++ drupal-8.6.7/modules/custom/idix_back/src/Form/IdixbackForm.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
  */
 class IdixbackForm extends FormBase
 {
-
     /**
      * {@inheritdoc}
      */
@@ -28,26 +27,30 @@ class IdixbackForm extends FormBase
      */
     public function buildForm(array $form, FormStateInterface $form_state)
     {
+        /***** Champs 'name' *****/
         $form['name'] = array(
             '#type' => 'textfield',
             '#title' => t('Name'),
             '#required' => TRUE,
         );
 
+        /** On récupère tous les 'nids' de type 'idix_back_film' **/
         $nids = db_select('node', 'n')
             ->fields('n', array('nid'))
             ->condition('type', 'idix_back_film', '=')
             ->execute()
             ->fetchCol();
 
-        // Get all of the article nodes.
+        /** On récupère toutes les données des noeuds correspodants aux 'nids' précédemment récupérés **/
         $nodes = node_load_multiple($nids);
 
+        /** On récupère les noms des films du type de contenu 'idix_back_film' **/
         $titles = [];
         foreach ($nodes as $key => $value) {
             $titles[$key] = $value->title->value;
         }
 
+        /***** Champs 'films' *****/
         $form['films'] = [
             '#type' => 'checkboxes',
             '#title' => 'Films',
@@ -55,6 +58,7 @@ class IdixbackForm extends FormBase
             '#options' => $titles,
         ];
 
+        /***** Champs 'submit' *****/
         $form['actions']['#type'] = 'actions';
         $form['actions']['submit'] = [
             '#type' => 'submit',
@@ -82,9 +86,13 @@ class IdixbackForm extends FormBase
     {
         drupal_set_message($this->t('@number a été ajouté avec succés !', ['@number' => $form_state->getValue('name')]));
 
+        /** Création d'un nouveau noeud de type 'idix_back_personnage' **/
         $my_article = Node::create(['type' => 'idix_back_personnage']);
+
+        /** On lui attribue la valeur du champ 'name' précédemment rentrée dans le formulaire **/
         $my_article->set('title', $form_state->getValue('name'));
 
+        /** Stockage des films précédemment cochés dans la variable '$films' **/
         $films = [];
         foreach ($form_state->getValue('films') as $key => $value) {
             if (!empty($value)) {
@@ -92,40 +100,56 @@ class IdixbackForm extends FormBase
             }
         }
 
+        /** On lui attribue la valeur du champ 'name' précédemment rentrée dans le formulaire **/
         $my_article->set('field_idix_back_personnage_name', $form_state->getValue('name'));
         $my_article->set('field_idix_back_personnage_films', $films);
 
+        /** Enregistrement du nouveau noeud **/
         $my_article->enforceIsNew();
         $my_article->save();
 
+        /** On récupère tous les 'nids' de type 'idix_back_personnage' **/
         $nids = db_select('node', 'n')
             ->fields('n', array('nid'))
             ->condition('type', 'idix_back_personnage', '=')
             ->execute()
             ->fetchCol();
 
+        /** On récupère la valeur du dernier 'nid' créé de type 'idix_back_personnage' **/
         $new_nid = end($nids);
 
+        /** Modification des personnages dans les films déjà présents **/
         foreach ($films as $key => $value) {
+            /** On charge les films 1 par 1 précédemment cochés dans le formulaire **/
             $node = \Drupal\node\Entity\Node::load($value);
             $array = $node->toArray();
             $characters = [];
 
+            /** S'il y a déjà des personnages présents dans le film **/
             if (!empty($array['field_idix_back_film_personnages'])) {
                 $actualscharacters = $array['field_idix_back_film_personnages'];
 
                 foreach ($actualscharacters as $keys => $values) {
                     $characters[] = $values['target_id'];
                 }
+
+                /** On rajoute l'id du personnage nouvellement créé parmis ceux déjà présents **/
                 array_push($characters, $new_nid);
 
+                /** Mise à jour du noeud **/
                 $node->set('field_idix_back_film_personnages', $characters);
                 $node->save();
+
+                /** S'il n'y a encore aucun personnage présent dans le film **/
             } else {
+                /** On relie l'id du personnage nouvellement créé au film **/
+                /** Mise à jour du noeud **/
                 $node->set('field_idix_back_film_personnages', $new_nid);
                 $node->save();
             }
         }
+
+        /** Redirection vers la page du personnage nouvellement créé **/
         $path = Url::fromUserInput('/node/' . $new_nid)->toString();
         $response = new RedirectResponse($path);
         $response->send();
